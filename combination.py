2e81e98365817d08e7fb00675b7492fad6ab3173
Md. Al-Momin Mostafa <53275609+momin-mostafa@users.noreply.github.com>
2020-08-17 22:47:10 +0600

    Update combination.py

--- combination.py
+++ combination.py
@@ -1,14 +1,6 @@
 lst = [0,0,0,0,0,0,0,0,0,0]
-lst[0] = int(input("1st item of list : "))
-lst[1] = int(input("2nd item of list : "))
-lst[2] = int(input("3rd item of list : "))
-lst[3] = int(input("4rt item of list : "))
-lst[4] = int(input("5th item of list : "))
-lst[5] = int(input("6th item of list : "))
-lst[6] = int(input("7th item of list : "))
-lst[7] = int(input("8th item of list : "))
-lst[8] = int(input("9th item of list : "))
-lst[9] = int(input("10th item of list : "))
+for x in range(10):
+    lst[x] = int(input(f"enter {x} no item : "))
 print(lst)
 print('\n')
 sum = int(input("input the sum :"))
